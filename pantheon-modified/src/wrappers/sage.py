af40239d6bb30bfd9df0d9c77bb7d0557cf3a949
yhchaos <yhcchaos@gmail.com>
2024-05-27 20:59:43 +0800

    add submodule sage

--- pantheon-modified/src/wrappers/sage.py
+++ pantheon-modified/src/wrappers/sage.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
 
+#!/usr/bin/env python2
 from os import path
 from subprocess import check_call
 
@@ -16,8 +16,8 @@ def main():
     cc_repo1 = path.join(context.third_party_dir, 'sage')
     cc_repo = path.join(cc_repo1,'sage_rl')
     rl_fld = path.join(cc_repo, 'rl_module')
-    send_src = path.join(cc_repo1, 'sage.sh')
-    recv_src = path.join(cc_repo1, 'client.sh')
+    send_src = path.join(cc_repo, 'sage.sh')
+    recv_src = path.join(rl_fld, 'client')
 
     if args.option == 'setup':
         sh_cmd = './build.sh'
@@ -30,14 +30,13 @@ def main():
         return
 
     if args.option == 'sender':
-        cmd = [send_src, args.port, ' 50',' 150',' 10','0', "pure", rl_fld , args.comment,args.num_flows,args.bw,args.basetime_fld,args.bw2,args.trace_period]
+        cmd = [send_src, "0", args.port, "wired24", "wired24", "1", "300", args.bw, args.bw2, "7", "0", "dataset-gen", "1500", args.actor_id, "0", "1", "1", "0", "0", args.run_id, args.data_dir, args.flow_id]
         sys.stderr.write("...  ...  ... %s\n" % (cmd))
-
         check_call(cmd)
         return
 
     if args.option == 'receiver':
-        cmd = [recv_src, args.ip, ' 1 ' ,args.port, rl_fld]
+        cmd = [recv_src, args.ip, ' 1 ' ,args.port]
         check_call(cmd)
         return
 
